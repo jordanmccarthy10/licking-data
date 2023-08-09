f634f685564934d4e82c52d40f1a24faf8f26a1a
jordanmccarthy10 <135728411+jordanmccarthy10@users.noreply.github.com>
2023-08-09 11:27:44 -0400

    makes licking raster plot
    Requires you to run sections of read_intan in order and save the raster plot info section. Tell the code which file to open and it will automatically create a raster plot for that session, no other manual input required

--- LickingRaster.py
+++ LickingRaster.py
@@ -1,18 +1,20 @@
 
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
+import numpy as np
 
 #this takes input from the info for raster plot section of read_intan.m and requires manual input of which trials had water dispensed
 
 #CHANGE TITLES and also check the drink period array at the bottom!!!!
 
-raster_plot_title='Phox2B_#26 2023-0731' #change this as needed
-save_file_name = ('/Users/jordanmccarthy/Desktop/licking/Phox2B_#26/20230731/licking_raster') #pick save name
-mat_data_trials = loadmat('/Users/jordanmccarthy/Desktop/licking/Phox2B_#26/20230731/rasterplotinfo.mat') #tell it which file to load
+raster_plot_title='Phox2B_#25 2023-0719' #change this as needed
+save_file_name = ('/Users/jordanmccarthy/Desktop/licking_raster') #pick save name
+mat_data_trials = loadmat('/Users/jordanmccarthy/Desktop/Phox2b#250719rasterplotinfo.mat') #tell it which file to load
 
 Lick_timing = mat_data_trials['LickTimes']
 Cue_timing = mat_data_trials['Cues']
 Drink_timing = mat_data_trials['Water']
+Water_dispense = np.squeeze(mat_data_trials['WaterDispense'])
 
 
 plt.figure(figsize=(16,9)) #to make it big enough to see properly
@@ -28,11 +30,8 @@ plt.eventplot(Cue_timing, color='magenta', linelength=1)
 #for y, drink_period in enumerate(Drink_timing):
     #plt.hlines(y, drink_period, drink_period+10, color='green', alpha=0.3, linewidth=1) 
 
-#this puts the drinking period only where the mouse drank - did this manually (trials=y), need to make a coding solution
-
-#y=list(range(1,101))+list(range(102,104))  #not inclusive of the last number so if you need up to 102, you would put 103
-y=[] #define this or use the above for a continuous list of numbers
-
+#this puts the drinking period only where the mouse drank 
+y=Water_dispense
 i=0
 for drink_period in Drink_timing:
   plt.hlines(y[i]-1, drink_period, drink_period+10, color='green', alpha=0.3, linewidth=1) 
