d69bd975b56ae1b6cbb73e31cb836123e03a650e
jordanmccarthy10 <135728411+jordanmccarthy10@users.noreply.github.com>
2023-08-09 09:57:41 -0400

    makes licking raster from read_intan.m
    currently requires manual input of when the water was dispensed - which is when SampleDrinkPeriods is NOT 3501 x 1.

--- LickingRaster.py
+++ LickingRaster.py
@@ -2,14 +2,18 @@
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
+#this takes input from the info for raster plot section of read_intan.m and requires manual input of which trials had water dispensed
 
-raster_plot_title='Phox2B_#23 2023-06-27' #change this as needed
-save_file_name = ('/Users/jordanmccarthy/Desktop/licking/Phox2B_#23/20230627/licking_raster') #pick save name
-mat_data_trials = loadmat('/Users/jordanmccarthy/Desktop/licking/Phox2B_#23/20230627/rasterplotinfo.mat') #tell it which file to load
+#CHANGE TITLES and also check the drink period array at the bottom!!!!
+
+raster_plot_title='Phox2B_#26 2023-0731' #change this as needed
+save_file_name = ('/Users/jordanmccarthy/Desktop/licking/Phox2B_#26/20230731/licking_raster') #pick save name
+mat_data_trials = loadmat('/Users/jordanmccarthy/Desktop/licking/Phox2B_#26/20230731/rasterplotinfo.mat') #tell it which file to load
 
 Lick_timing = mat_data_trials['LickTimes']
 Cue_timing = mat_data_trials['Cues']
 Drink_timing = mat_data_trials['Water']
+
 
 plt.figure(figsize=(16,9)) #to make it big enough to see properly
 
@@ -20,10 +24,19 @@ for trials in range(len(Lick_timing)):
 #plots cues
 plt.eventplot(Cue_timing, color='magenta', linelength=1)
 
-#plots drinking periods as highlighted section
-for y, drink_period in enumerate(Drink_timing):
-    plt.hlines(y, drink_period, drink_period+10, color='green', alpha=0.3, linewidth=1) 
+#plots drinking periods as highlighted section - this works if mouse drank in every trial
+#for y, drink_period in enumerate(Drink_timing):
+    #plt.hlines(y, drink_period, drink_period+10, color='green', alpha=0.3, linewidth=1) 
 
+#this puts the drinking period only where the mouse drank - did this manually (trials=y), need to make a coding solution
+
+#y=list(range(1,101))+list(range(102,104))  #not inclusive of the last number so if you need up to 102, you would put 103
+y=[] #define this or use the above for a continuous list of numbers
+
+i=0
+for drink_period in Drink_timing:
+  plt.hlines(y[i]-1, drink_period, drink_period+10, color='green', alpha=0.3, linewidth=1) 
+  i += 1
 
 plt.title(raster_plot_title) 
 plt.ylabel('Trial')
